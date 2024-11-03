faa593f047e5f4c418a0a7662232e0ff407e7a4f
Ivan Vidal Roig <136822125+IvanVidal08@users.noreply.github.com>
2024-11-03 18:45:27 +0100

    TotElPersonatje

--- SuperHell/Assets/Scripts/PlayerMovement.cs
+++ SuperHell/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // Variables públicas para ajustar en el Inspector
+    public float speed = 5f;           
+    public float runSpeed = 8f;         
+    public float rotationSpeed= 250;
+    public Animator animator;
+    public Rigidbody rb;
+    public float jumpHeight = 3;
+    public Transform groundCheck;
+    public float groundDistance = 0.1f;
+    public LayerMask groundMask;
+    bool isGrounded;
 
-    // Update is called once per frame
+    private float x,y;
     void Update()
     {
-        
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+        transform.Rotate(0, x * Time.deltaTime * rotationSpeed, 0);
+        transform.Translate(0,0, y * Time.deltaTime * runSpeed);
+        animator.SetFloat("VelX", x);
+        animator.SetFloat("VelY", y);
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (Input.GetKey("space") && isGrounded){
+            animator.Play("Jump");
+            Invoke("Jump", 0);
+        }
+
+
     }
+    public void Jump(){
+            rb.AddForce(Vector3.up*jumpHeight, ForceMode.Impulse);    
+    }     
 }
